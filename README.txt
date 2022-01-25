86b3838b322209e6e3bbf86ee8506bdb5d50e906
Heigan <heigan11@gmail.com>
2022-01-25 21:18:27 +0300

    first servlet are deployed

---
+++ README.txt
@@ -0,0 +1,2 @@
+mvn clean install
+mvn org.codehaus.cargo:cargo-maven2-plugin:run
---
+++ src/main/java/edu/school21/cinema/servlets/SignInServlet.java
@@ -0,0 +1,30 @@
+package edu.school21.cinema.servlets;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/signIn")
+public class SignInServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello!\n");
+    }
+
+    @Override
+    public void destroy() { }
+}
