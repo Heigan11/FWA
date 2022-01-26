88a7a48b19af7136afdc662f7cca634f808384c5
Heigan <heigan11@gmail.com>
2022-01-27 00:21:13 +0300

    work with jdbc

---
+++ src/main/java/edu/school21/cinema/config/SpringConfig.java
@@ -0,0 +1,43 @@
+package edu.school21.cinema.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("edu.school21.cinema")
+@PropertySource("../application.properties")
+public class SpringConfig {
+
+    @Value("${spring.datasource.url}")
+    private String datasource_url;
+    @Value("${spring.datasource.username}")
+    private String datasource_username;
+    @Value("${spring.datasource.password}")
+    private String datasource_password;
+    @Value("${spring.datasource.driver.name}")
+    private String datasource_driver_name;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(datasource_driver_name);
+        dataSource.setUrl(datasource_url);
+        dataSource.setUsername(datasource_username);
+        dataSource.setPassword(datasource_password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
---
+++ src/main/java/edu/school21/cinema/models/User.java
@@ -0,0 +1,95 @@
+package edu.school21.cinema.models;
+
+import java.util.Objects;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String phone;
+    private String email;
+    private String password;
+
+    public User(Long id, String name, String surname, String phone, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) && name.equals(user.name) && surname.equals(user.surname) && phone.equals(user.phone) && email.equals(user.email) && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, phone, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
